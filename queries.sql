49f59d7eb57932af427c8b68493a9ff90a6cba9c
JWHanna <64369647+JWHanna@users.noreply.github.com>
2020-07-21 15:02:43 -0400

    Adding new tables created via join.

--- queries.sql
+++ queries.sql
@@ -45,3 +45,62 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+-- Create a new table for retireing employees.
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+--Check the table.
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+-- The SELECT statement selects only the columns we want to view from each table.
+-- The FROM statement points to the first table to be joined, Departments (Table 1).
+-- INNER JOIN points to the second table to be joined, dept_manager (Table 2).
+-- ON departments.dept_no = managers.dept_no; indicates where Postgres should look for 
+-- matches.
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date 
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+DROP TABLE current_emp;
+-- Use left join for retirement_info and dept_emp tables 
+-- and create a new table (current_emp) for the data.
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), 
+	de.dept_no
+INTO current_emp_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
