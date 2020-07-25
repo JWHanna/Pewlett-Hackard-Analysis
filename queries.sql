87ef9fd201bf8b3742f4fe818708ca4f7a440ba6
JWHanna <64369647+JWHanna@users.noreply.github.com>
2020-07-25 14:51:14 -0400

    Updates to queries.sql and adding additional tables.

--- queries.sql
+++ queries.sql
@@ -1,3 +1,84 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments(
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+-- Code Breakdpwn:
+-- 1. "CREATE TABLE" is the syntax required to create a new table in SQL.
+-- 2. "departments" is the name of the table and how it will be referenced in queries.
+-- 3. "dept_no VARCHAR(4) NOT NULL", creates a column named “dept_no” that can hold up
+--    to four varying characters, while NOT NULL tells SQL that no null fields will 
+--    be allowed when importing data.
+-- 4. "dept_name VARCHAR(40) NOT NULL", creates a column similar to the dept_no, 
+--    only the varying character count has a maximum of 40.
+-- 5. "PRIMARY KEY (dept_no)", means that the dept_no column is used as the primary 
+--    key for this table.
+-- 6. "UNIQUE (dept_name)" adds the unique constraint to the dept_name column.
+
+CREATE TABLE employees(
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_manager(
+dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+-- 1. The FOREIGN KEY constraint tells Postgres that there is a link between two tables
+-- 2. The parentheses following FOREIGN KEY specify which of the current table’s columns 
+--    is linked to another table
+-- 3. REFERENCES table_name (column_name) tells Postgres which other table uses that 
+--    column as a primary key
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+
+DROP TABLE dept_emp;
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
+);
+
+
+SELECT * FROM departments;
+-- The SELECT statement tells Postgres that we’re about to query the database.
+-- The asterisk tells Postgres that we’re looking for every column in a table.
+-- FROM departments tells pgAdmin which table to search.
+-- The semicolon signifies the completion of the query.
+
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
@@ -7,7 +88,6 @@ WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
 -- - FROM employees tells SQL in which of the six tables to look.
 -- - The WHERE clause brings up even more specifics. We want SQL to look in the birth_date
 --   column for anyone born between January 1, 1952, and December 31, 1955.
-
 
 SELECT first_name, last_name
 FROM employees
@@ -37,17 +117,8 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
--- Create a table for employees eleigible for retirement
-SELECT first_name, last_name
-INTO retirement_info
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
-SELECT * FROM retirement_info;
-
 DROP TABLE retirement_info;
--- Create a new table for retireing employees.
+-- Create a table for retireing employees.
 SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
@@ -104,3 +175,77 @@ LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
+
+-- List 1: Employee Information
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+	 
+-- List 2: Management
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+		dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);	
+
+-- List 3: Department Retirees
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name	
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- Sales Retirees
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name	
+INTO sales_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE (d.dept_name = 'Sales');
+
+-- Sales and Development ritirees
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name	
+INTO mentors_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
